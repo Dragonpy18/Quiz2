6abf510844c23e4e84ad360c7abacb0017a499b5
Arav Ravula <arav.ravula@gmail.com>
2023-12-07 12:01:13 -0500

    Edited main
    Removed print statement for testing

--- Main.java
+++ Main.java
@@ -1,7 +1,6 @@
 public class Main{
     public static void main(String[] args){
         Adder adder = (int a, int b) -> (b+a)/2 * (b-a+1); 
-        System.out.println((3+1)/2 * (3-1+1));
     }
 
 }
